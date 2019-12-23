d7a98d9c924dd46319cfbd9523d5b186922b150b
Henry Xu <henry.xu@ringcentral.com>
2019-12-23 22:05:42 +0800

    chore(py-bombbag): setup python project

---
+++ py-bombbag/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bombbag", # Replace with your own username
+    version="0.0.1",
+    author="Weihong Xu",
+    author_email="xuweihong.cn@gmail.com",
+    description="personal toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/link89/bomb-bag",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
